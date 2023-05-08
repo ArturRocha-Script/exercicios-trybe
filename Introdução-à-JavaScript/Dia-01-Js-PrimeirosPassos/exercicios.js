2693a44e931e3af57bf1cbb7de7647ac51158e75
Artur Rocha <artur.nunes.rocha@hotmail.com>
2023-05-08 00:43:12 -0300

    exercicio 02

--- Introdução-à-JavaScript/Dia-01-Js-PrimeirosPassos/exercicios.js
+++ Introdução-à-JavaScript/Dia-01-Js-PrimeirosPassos/exercicios.js
@@ -1,7 +1,12 @@
 // exercicios 01
 let a = 8;
 let b = 15;
-console.log(a+b, a-b, a*b, a/b, a%b);
+//console.log(a+b, a-b, a*b, a/b, a%b);
 //--------------------------------------------------------------
 // exercicios 02
-
+if (a > b) {
+    console.log('a é maior, Uhull!!');
+} else {
+    console.log('b é maior, que pena!');
+}
+//--------------------------------------------------------------
