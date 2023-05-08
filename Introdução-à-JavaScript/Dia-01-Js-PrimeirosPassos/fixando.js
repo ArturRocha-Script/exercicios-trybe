8d42c16e6c013d64f74e48c8bb3f9156fa455e0d
Artur Rocha <artur.nunes.rocha@hotmail.com>
2023-05-08 00:03:20 -0300

    iniciando no js

---
+++ Introdução-à-JavaScript/Dia-01-Js-PrimeirosPassos/fixando.js
@@ -0,0 +1,72 @@
+// fixando variaveis
+// let myName = 'Artur';
+// let birthCity = 'Rio de Janeiro';
+// let birthYear = 1994;
+//console.log(myName, birthCity, birthYear);
+//----------------------------------------------------------------------
+//Fixando tipos primitivos, tipagem dinâmica e operações aritméticas
+// let base = 5;
+// let height = 8;
+// let area = base * height;
+// let perimeter = base * 2 + height * 2;
+//console.log(area, perimeter);
+//----------------------------------------------------------------------
+// fixando If e else
+//let note = 28;
+// if (note >= 80) {
+//     console.log("Parabéns, você faz parte do grupo de pessoas aprovadas!");
+// } else if (note < 80 && note >= 60) {
+//     console.log("Você está na nossa lista de espera");
+// } else {
+//     console.log("Infelizmente, você reprovou");
+// }
+//----------------------------------------------------------------------
+//Fixando operadores lógicos
+// Operador && (AND)
+// let currentHour = 20;
+// let message = "";
+// if (currentHour >= 22) {
+//     console.log("Não deveríamos comer nada, é hora de dormir");
+// } else if (currentHour >= 18 && currentHour < 22) {
+//     console.log("Rango da noite, vamos jantar :D");
+// } else if (currentHour >= 14 && currentHour < 18 ) {
+//     console.log("Vamos fazer um bolo pro café da tarde?");
+// } else if (currentHour >= 11 && currentHour < 14) {
+//     console.log("Hora do almoço!!!");
+// } else {
+//     console.log("Hmmm, cheiro de café recém-passado");
+// }
+//----------------------------------------------------------------------
+// Operador || (OR)
+// let weekDay = 'quarta-feira';
+// if (weekDay = 'segunda-feira' || 'terça-feira' || 'quarta-feira' || 'qaunta-feira' || 'sexta-feira') {
+//     console.log('Oba, mais um dia de aprendizado na Trybe >:D');
+// } else {
+//     console.log('FINALMENTE, descanso merecido, Ko tem descanso não :(');
+// }
+//----------------------------------------------------------------------
+// Operador ! (NOT)
+// let squirtle = "melhor pokemon inicial";
+// console.log(!squirtle);
+// console.log(!42);
+// console.log(!0); // true
+// O número 0 tem o valor "falsy" no JavaScript. Logo, seu oposto é true.
+//-----------------------------------------------------------------------
+// Fixando switch/case
+// let status = 'reprovada';
+// switch (status) {
+//     case 'aprovoda':
+//     console.log('Parabéns, você está no grupo de pessoas aprovadas!');
+//     break;
+
+//     case 'lista':
+//         console.log('Você está na nossa lista de espera.');
+//         break;
+
+//     case 'reprovada':
+//         console.log('Infelizmente, você reprovou.');    
+//         break;
+
+//     default:
+//         console.log('Informação incorreta.');    
+// }
