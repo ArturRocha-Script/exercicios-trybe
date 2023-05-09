aa7f99dd802010317e7fe05824f999c11a5b5323
Artur Rocha <artur.nunes.rocha@hotmail.com>
2023-05-09 00:54:39 -0300

    exercício 08

--- Introdução-à-JavaScript/Dia-01-Js-PrimeirosPassos/exercicios.js
+++ Introdução-à-JavaScript/Dia-01-Js-PrimeirosPassos/exercicios.js
@@ -66,18 +66,25 @@ let chessPiece = 'bispo';
 //---------------------------------------------------------
 // Exercício 07
 let nota = 88;
-if (nota < 0 || nota > 100) {
-    console.log('Erro, você foi desclassificado');
-} else if (nota >= 90) {
-    console.log('A');
-} else if (nota >=80) {
-    console.log('B');
-} else if (nota >= 70) {
-    console.log('C');
-} else if (nota >= 60) {
-    console.log('D');
-}else if (nota >= 50) {
-    console.log('E');
-}else {
-    console.log('F');
+// if (nota < 0 || nota > 100) {
+//     console.log('Erro, você foi desclassificado');
+// } else if (nota >= 90) {
+//     console.log('A');
+// } else if (nota >=80) {
+//     console.log('B');
+// } else if (nota >= 70) {
+//     console.log('C');
+// } else if (nota >= 60) {
+//     console.log('D');
+// }else if (nota >= 50) {
+//     console.log('E');
+// }else {
+//     console.log('F');
+// }
+//--------------------------------------------------------
+// Exercício 08
+if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
+    console.log(true);
+} else {
+    console.log(false);
 }
